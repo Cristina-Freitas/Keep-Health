19b6911c517d6b9a468e60a1682d1a77178553e2
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-03-23 15:41:17 -0300

    [M1S09] Ex. 3 - Criação tela login - parte 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login = {
+    emailUsuario: '',
+    senha: ''
+  };
+
+  constructor(private router: Router ){}
+
+  entrar(){
+    if(this.login.emailUsuario && this.login.senha){
+      this.router.navigate(['/home']);
+    }else{
+      window.alert('Digite seu email e senha para prosseguir!')
+    }
+  }
 
 }
