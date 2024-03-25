1cad11b36e2c90007756dfbad82d042e5e3f9f3b
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-03-25 13:09:07 -0300

    [M1S09] Ex. 4 - Criação tela login e [M1S09] Ex. 5 - Criação tela cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  cadastro = {
+    nomeUsuario: '',
+    emailUsuario: '',
+    dataNascimentoUsuario: '',
+    senhaUsuario: '',
+    confirmarSenhaUsuario: ''
+  };
 
-}
+  constructor(private router: Router){}
+
+  camposValidos(): boolean {
+    return !!(
+           this.cadastro.nomeUsuario && 
+           this.cadastro.emailUsuario && 
+           this.cadastro.dataNascimentoUsuario && 
+           this.cadastro.senhaUsuario &&
+           this.cadastro.confirmarSenhaUsuario
+    );
+  };
+
+  senhasIguais(): boolean {
+    return this.cadastro.senhaUsuario === this.cadastro.confirmarSenhaUsuario
+    
+  }
+  cadastrar(){
+    if(this.camposValidos() && this.senhasIguais()){
+       
+       let usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+        usuarios.push(this.cadastro);
+        localStorage.setItem('usuarios', JSON.stringify(usuarios)); 
+        this.router.navigate(['']);
+    }else{
+      window.alert('Preencha todos os campos corretamente e verifique se as senhas conferem para prosseguir!')
+    }
+  }
+
+  }
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,11 +20,36 @@ export class LoginComponent {
   constructor(private router: Router ){}
 
   entrar(){
-    if(this.login.emailUsuario && this.login.senha){
+const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+console.log('Email fornecido:', this.login.emailUsuario);
+console.log('Senha fornecida:', this.login.senha);
+const encontrado = usuarios.find((usuario: any) => {
+  console.log('Email armazenado:', usuario.emailUsuario);
+  console.log('Senha armazenada:', usuario.senhaUsuario);
+  return usuario.emailUsuario === this.login.emailUsuario && usuario.senhaUsuario === this.login.senha
+});
+
+    if(encontrado){
       this.router.navigate(['/home']);
     }else{
-      window.alert('Digite seu email e senha para prosseguir!')
+      window.alert('Usuário ou senha inválidos!')
     }
+  }
+
+  esqueciAsenha(){
+    const senhaPadrao = 'a1b2c4d4';
+    const usuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+    const encontrado = usuarios.find((usuario: any) => 
+      usuario.emailUsuario === this.login.emailUsuario
+    );
+    if(encontrado){
+      encontrado.senhaUsuario = senhaPadrao;
+      localStorage.setItem('usuarios', JSON.stringify(usuarios));
+      window.alert('Sua senha foi alterada para a senha padrão: '+ senhaPadrao);
+    }else{
+      window.alert('Usuário não encontrado!');
+    }
+    
   }
 
 }
