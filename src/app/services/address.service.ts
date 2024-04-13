a8043318cf851741190f55f502a3520018486e4c
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-04-12 22:02:03 -0300

    [M1S10] Ex 06-Criação do service address e do método get() para a API viaCep:

---
+++ src/app/services/address.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAddress(cep:string = ''){
+    let url = 'https://viacep.com.br/ws/';
+    let headers = {
+      headers: {'Content-Type': 'application/json'}
+    };
+    return this.httpClient.get(`${url}/${cep}/json`, headers);
+  }
+}
