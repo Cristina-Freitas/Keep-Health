52e9a1d973d9546183e428346def775ed6e7500a
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-04-15 19:47:45 -0300

    [M1S11] Ex. 5 - Home - parte 1: select, datepicker e botões.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { SharedModule } from '../../shared/shared.module';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +13,135 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [SharedModule,
             LoginComponent,
             RouterLink,
-            CommonModule
-  ],
+            CommonModule,
+            DialogModule,
+            ButtonModule,
+            ReactiveFormsModule,
+            FormsModule,
+            ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
+
 })
 export class HomeComponent {
 
+  visible: boolean = false;
+  showDialog() {
+    this.visible = true;
+    this.novaAtividade.reset();
+    this.editarAtividade = null;
+  }
+
+  listaDeTreinos = this.gettreinos();
+
+  novaAtividade = new FormGroup({
+    title: new FormControl('', Validators.required),
+    type: new FormControl('', Validators.required),
+    date: new FormControl('', Validators.required),
+    distance: new FormControl(0),
+    time: new FormControl(''),
+  });
+
+  tiposDeAtividades = [
+    {name: "Caminhada"},
+    {name: "Corrida"},
+    {name: "Natação"},
+    {name: "Bicicleta"},
+    {name: "Dança"},
+    {name: "Musculação"},
+    {name: "Crossfit"}
+  ];
+
+  editarAtividade: any = null;
+
+  salvarAtividade() {
+    if (this.novaAtividade.value.title && this.novaAtividade.value.type && this.novaAtividade.value.date) {
+      let treinos = this.gettreinos();
+
+      let distance = typeof this.novaAtividade.value.distance === 'number' ? this.novaAtividade.value.distance : 0;
+
+      if (this.editarAtividade) {
+
+        let index = treinos.findIndex((treino: { title: any; type: any; date: any; distance: any; time: any; }) => 
+          treino.title == this.editarAtividade.title &&
+          treino.type == this.editarAtividade.type &&
+          treino.date == this.editarAtividade.date &&
+          treino.distance == this.editarAtividade.distance &&
+          treino.time == this.editarAtividade.time
+        );
+        if (index > -1) {
+          treinos[index] = this.novaAtividade.value;
+        }
+        localStorage.setItem("Atividades", JSON.stringify(treinos));
+      } else {
+        // Adicionar novo exercício:
+      this.addAtividade(this.novaAtividade.value.title, this.novaAtividade.value.type, this.novaAtividade.value.date, distance, this.novaAtividade.value.time);
+      this.novaAtividade.reset();
+      }
+      this.visible = false;
+      this.listaDeTreinos = this.gettreinos();
+      this.editarAtividade = null;
+    }
+    else {
+      alert("Preencha todos os campos obrigatórios.");
+    }
+  };
+
+  gettreinos() {
+    const emptyDatabase: string[] = [];
+    const treinos = localStorage.getItem("Atividades");
+    if (!!treinos) {
+      let parsedtreinos = JSON.parse(treinos);
+
+      // ordenar em ordem cronológica, por data
+      parsedtreinos.sort((a: any, b: any) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      return parsedtreinos;
+    } else {
+      localStorage.setItem("Atividades", JSON.stringify(emptyDatabase));
+      return [];
+    };
+  };
+
+  addAtividade(title: string, type: string, date: string, distance: number, time: any) {
+    const novaAtividade = {
+      title: title,
+      type: type,
+      date: date,
+      distance: distance,
+      time: time,
+    };
+    let treinos = this.gettreinos();
+    treinos.push(novaAtividade);
+    localStorage.setItem("Atividades", JSON.stringify(treinos));
+  };
+
+  // Chama a funcionalidade de editar o exercício através do card:
+  atividade(treino: any) {
+    this.editarAtividade = treino;
+    this.novaAtividade.setValue(treino);
+    this.visible = true;
+  };
+
+  deletarAtividade() {
+    if (this.editarAtividade) {
+      if (confirm("Tem certeza que deseja apagar essa atividade? Esta ação será irreversível.")) {
+      let treinos = this.gettreinos();
+      let index = treinos.findIndex((treino: { title: any; type: any; date: any; distance: any; time: any; }) => 
+        treino.title === this.editarAtividade.title &&
+        treino.type === this.editarAtividade.type &&
+        treino.date === this.editarAtividade.date &&
+        treino.distance === this.editarAtividade.distance &&
+        treino.time === this.editarAtividade.time
+      );
+      if (index > -1) {
+        treinos.splice(index, 1);
+        localStorage.setItem("Atividades", JSON.stringify(treinos));
+      }
+      this.novaAtividade.reset();
+      this.editarAtividade = null;
+      this.visible = false;
+      this.listaDeTreinos = this.gettreinos();
+    }
+    }
+  };
 }
